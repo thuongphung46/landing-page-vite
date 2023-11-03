3382e1c6f68ddae95e29026ac58d12e197681c07
Phung Hoai Thuong <thuongphung46@gmail.com>
2023-11-04 02:37:24 +0700

    fix navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,23 @@ import { logo } from '../assets'
 import {FaFacebookF,FaDribbble,FaLinkedinIn,FaInstagram,FaBehance} from 'react-icons/fa'
 
 const Footer = () => {
+    const List_icons=[
+        {
+            icon:<FaFacebookF size={25} style={{color:'#4DC39E'}} />,
+        },
+        {
+            icon:<FaDribbble size={25} style={{color:'#4DC39E'}} />,
+        },
+        {
+            icon:<FaLinkedinIn size={25} style={{color:'#4DC39E'}} />,
+        },
+        {
+            icon:<FaInstagram size={25} style={{color:'#4DC39E'}} />,
+        },
+        {
+            icon:<FaBehance size={25} style={{color:'#4DC39E'}} />,
+        }
+    ]
   return (
     <div className='w-full bg-white py-24'>
         <div className='md:max-w-[1480px] m-auto grid md:grid-cols-5 max-[780px]:grid-cols-2  gap-8 max-w-[600px]  px-4 md:px-0'>
@@ -14,17 +31,14 @@ const Footer = () => {
                 <h3 className='py-2 text-[#6D737A]'>Praesent nulla massa, hendrerit <br></br> vestibulum gravida in, feugiat auctor felis.</h3>
                 <h3 className='py-2 text-[#363A3D]'>Email: example@mail.com</h3>
                 <div className='flex gap-4 py-4'>
-                        <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaFacebookF size={25} style={{color:'#4DC39E'}} /></div>
-                        <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaDribbble size={25} style={{color:'#4DC39E'}} /></div>
-                        <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaLinkedinIn size={25} style={{color:'#4DC39E'}} /></div>
-                        <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaInstagram size={25} style={{color:'#4DC39E'}} /></div>
-                        <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaBehance size={25} style={{color:'#4DC39E'}} /></div>
-
+                {List_icons.map((item,index)=>(
+                    <div key={index} className='hover:text-[#20B486] cursor-pointer'>{item.icon}</div>
+                ))}
                 </div>
 
             </div>
 
-            <div>
+            {/* <div>
                 <h3 className='text-2xl font-bold'>Explore</h3>
                 <ul className='py-6 text-[#6D737A]'>
                     <li className='py-2'>Home</li>
@@ -34,7 +48,7 @@ const Footer = () => {
                     <li className='py-2'>Contact</li>
 
                 </ul>
-            </div>
+            </div> */}
 
             <div>
                 <h3 className='text-2xl font-bold'>Category</h3>
