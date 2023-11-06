271387787d04fc2043d491e5088ae5114079957a
Phung Hoai Thuong <thuongphung46@gmail.com>
2023-11-06 22:56:02 +0700

    add slide mess and đi ngủ

---
+++ src/data/UrlImages.js
@@ -0,0 +1,64 @@
+import {
+  Image1,
+  Image10,
+  Image11,
+  Image12,
+  Image13,
+  Image14,
+  Image15,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Image7,
+  Image8,
+  Image9,
+} from "../assets";
+export const DataMessImg = [
+  {
+    url: Image1,
+  },
+  {
+    url: Image2,
+  },
+  {
+    url: Image3,
+  },
+  {
+    url: Image4,
+  },
+  {
+    url: Image5,
+  },
+  {
+    url: Image6,
+  },
+  {
+    url: Image7,
+  },
+  {
+    url: Image8,
+  },
+  {
+    url: Image9,
+  },
+  {
+    url: Image10,
+  },
+  {
+    url: Image11,
+  },
+  {
+    url: Image12,
+  },
+  {
+    url: Image13,
+  },
+  {
+    url: Image14,
+  },
+  {
+    url: Image15,
+  },
+];
